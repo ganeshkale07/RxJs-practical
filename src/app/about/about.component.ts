279c03a717732534047079f89dd1afa9f426cd90
KALE <ggkale07@gmail.com>
2024-01-22 17:38:15 +0530

    Basic template for rxjs course

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, fromEvent, interval, noop } from "rxjs";
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    //WHAT ARE STREAMS
+
+    //streams === observables
+    //create blue print of streams
+
+    //Blur print - definition - declaration --> They know now what to do
+    const interval$ = interval(1000); //Hanuman
+    //Just blurprint or definition not enough
+    //they do not know what to do with it 
+
+    //when jamwant tell hanuman you can do that
+    //interval$.subscribe((val) => console.log(val));
+
+    //If we have to create stream of click events
+    let mouseClick$ = fromEvent(document, 'click');
+    mouseClick$.subscribe(evt => console.log(evt));
+
+    //Creating My own HTTP observable 😀
+
+    const courses$ = new Observable(observer => {
+      fetch('http://localhost:9090/api/courses')
+        .then(response => response.json())
+        .then(body => {
+          observer.next(body);
+
+          observer.complete();
+        })
+        .catch(err => observer.error(err))
+
+    })
+
+    courses$.subscribe(
+      courses => console.log(courses),
+      //If do not have error handling for now
+      //so () => {} instead of writing these 
+      //we will say no operation noop - in short
+      noop,
+      () => console.log("Completed")
+    )
+
+
+  }
+
+}
